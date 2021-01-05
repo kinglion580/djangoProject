4b9fdfefcb68ca9740fc5a3195db259ec9fea607
kinglion580 <1609019405@qq.com>
2021-01-05 06:43:02 -0500

    解决 daphne 运行错误

--- djangoProject/asgi.py
+++ djangoProject/asgi.py
@@ -7,11 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.sessions import SessionMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+django_asgi_app = get_asgi_application()
+
+from channels.sessions import SessionMiddlewareStack
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 import realtime.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
